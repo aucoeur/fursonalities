29567110ce70f891d3a1f71b656eb944d003eb13
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-06-15 16:19:01 -0700

    show basic stats view

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, SafeView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, SafeView, FlatList, StyleSheet, Text, View } from 'react-native';
 
 import Item from './Item'
-import { cats } from './breeds'
+import { dogs } from './breeds'
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.container}>
-          {cats.map(cat => <Item name={cat.breed}/>)}
+    <SafeAreaView style={styles.container}>
+        <View style={styles.listContainer}>
+          <FlatList
+            keyExtractor={item => item.breed}
+            data={dogs}
+            renderItem={({item, index}) => {
+              return <Item data={item} index={index}/>
+            }} />
           <StatusBar style="auto" />
         </View>
-      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  listContainer: {
+    width: '100%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
--- Item.js
+++ Item.js
@@ -2,17 +2,33 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 export default function Item(props) {
-  const { name } = props
+  const { data, index } = props
+  const { breed } = data
+  const keys = Object.keys(data).filter(key => key !== 'breed')
 
   return (
-    <View>
-      <Text style={style.text}>{name}</Text>
+    <View style={style.container}>
+      <Text style={style.head}>{index}: {breed}</Text>
+      {
+        keys.map(key =>
+            <Text>{key}{data[key]}</Text>
+        )
+      }
     </View>
   )
 }
 
 const style = StyleSheet.create({
-  text: {
-    color: '#f90',
+  head: {
+    fontSize: 35
+  },
+  stats: {
+    fontSize: 20,
+    marginLeft: 30,
+    justifyContent: "space-between"
+  },
+  container: {
+    padding: 10,
+    margin: 5,
   }
 })

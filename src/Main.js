0395ab184bf94375f031ef693e378c7fba8a3ddf
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-06-16 15:49:31 -0700

    add clearButton to search input

--- src/Main.js
+++ src/Main.js
@@ -11,53 +11,73 @@ export default function App() {
   return (
     <SafeAreaView style={style.container}>
       <StatusBar style="auto" />
-      <View style={style.search}>
-        <TextInput
-          style={style.searchText}
-          placeholder="Search"
-          onChangeText={setSearch}
-          value={search} />
-      </View>
-      <View style={style.listContainer}>
-        <FlatList
-          keyExtractor={item => item.breed}
-          data={
-            cats.filter(item =>
-              item.breed.includes(search) || item.breed.toLowerCase().includes(search)
-            )}
-          renderItem={({item, index}) => {
-            return <Item data={item} index={index}/>
-          }} />
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding": "height"}
+        style={style.kbavoid}
+      >
+
+        <View style={style.listContainer}>
+          <FlatList
+            keyExtractor={item => item.breed}
+            data={
+              cats.filter(item =>
+                item.breed.includes(search) || item.breed.toLowerCase().includes(search)
+              )}
+            renderItem={({item, index}) => {
+              return <Item data={item} index={index}/>
+            }} />
+        </View>
+        <View style={style.search}>
+          <TextInput
+            style={style.searchText}
+            clearButtonMode="always"
+            placeholder="Search"
+            onChangeText={setSearch}
+            value={search} />
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
+  kbavoid: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   listContainer: {
+    flexBasis: 100,
+    flexGrow: 1,
     width: '100%',
+    // paddingTop: 10,
+    paddingBottom: 10,
   },
   header: {
     color: '#f0f',
     fontSize: 50,
   },
   search: {
+    flexShrink: 0,
     width: '95%',
-    padding: 5,
-    marginTop: 50,
+    height: 40,
+    marginBottom: 2,
     backgroundColor: 'hsla(0, 0%, 0%, 0.08)',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 25,
+    // borderWidth: 1,
   },
   searchText: {
     fontSize: 24,
-
+    width: '100%',
+    textAlign: 'center'
   },
 });
